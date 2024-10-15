49fcffa0bcdca637d9a01210768ac8d9791034a9
Junyan <0014070@Junyan.local>
2024-10-15 15:28:08 +0800

    #today i worked on my model and started on my view #next I will test out my view and continue working on it

--- app/views.py
+++ app/views.py
@@ -3,3 +3,11 @@
 from banjo.urls import route_get, route_post
 from settings import BASE_URL
 
+@route_get(BASE_URL + 'all')
+def all_drills(args):
+    
+    for fortune in Fortune.objects.all():
+        fortunes_list.append(fortune.json_response())
+
+
+    
